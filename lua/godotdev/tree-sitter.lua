e84b9bf5e38084b073cccf1534b0ade88ac197be
mathijs-bakker <mathijs_bakker@me.com>
2026-03-17 09:06:07 +0100

    fix: drop empty gdshader parser override

--- lua/godotdev/tree-sitter.lua
+++ lua/godotdev/tree-sitter.lua
@@ -16,17 +16,3 @@ vim.filetype.add({
     gdshader = "gdshader",
   },
 })
-
-local parsers_ok, ts_parsers = pcall(require, "nvim-treesitter.parsers")
-if parsers_ok then
-  local parser_configs = ts_parsers.get_parser_configs()
-  parser_configs.gdshader = {
-    used_by = { "gdshader" }, -- filetype
-    install_info = {
-      url = "", -- no external parser
-      files = {}, -- no parser files
-      generate_requires_npm = false,
-      requires_generate_from_grammar = false,
-    },
-  }
-end
