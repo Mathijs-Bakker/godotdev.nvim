aec475f2c481b55c8d6c30e2d9e19e88ae758aaa
mathijs-bakker <mathijs_bakker@me.com>
2026-03-17 08:37:22 +0100

    fix: handle formatter_cmd with arguments

--- lua/godotdev/formatting.lua
+++ lua/godotdev/formatting.lua
@@ -3,16 +3,20 @@ local config = require("godotdev").opts
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.gd",
   callback = function()
-    local exe = config.formatter_cmd or config.formatter
-    if vim.fn.executable(exe) ~= 1 then
-      vim.notify(exe .. " not found in PATH. Run `:checkhealth godotdev` for more info.", vim.log.levels.WARN)
+    local cmd = config.formatter_cmd or config.formatter
+    local bin = cmd
+    if cmd:find("%s") then
+      bin = vim.split(cmd, "%s+")[1]
+    end
+    if vim.fn.executable(bin) ~= 1 then
+      vim.notify(bin .. " not found in PATH. Run `:checkhealth godotdev` for more info.", vim.log.levels.WARN)
       return
     end
 
     if config.formatter == "gdformat" then
-      vim.cmd("silent !" .. exe .. " %")
+      vim.cmd("silent !" .. cmd .. " %")
     elseif config.formatter == "gdscript-format" then
-      vim.cmd("silent !" .. exe .. " %")
+      vim.cmd("silent !" .. cmd .. " %")
     end
 
     vim.cmd("checktime")
